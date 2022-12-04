56b5a5dade3febd657fb11230e263ab19210bf2f
dldnjswns31 <dldnjswns32@gmail.com>
2022-12-04 20:06:59 +0900

    project init

---
+++ src/style/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  cardBgColor: "transparent",
+  textColor: "white",
+  accentColor: "#9c88ff",
+};
